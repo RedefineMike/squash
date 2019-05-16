8b5610e7d7742b0d79db279a95d24dee70938b15
Mike Kelp <mikekelp@gmail.com>
2019-05-16 12:06:43 -0700

    MySQL : Implemented DATE_ADD and DATE_SUB functions

--- squash-mysql/test/org/jetbrains/squash/dialects/mysql/tests/MySqlQueryTests.kt
+++ squash-mysql/test/org/jetbrains/squash/dialects/mysql/tests/MySqlQueryTests.kt
@@ -1,10 +1,33 @@
 package org.jetbrains.squash.dialects.mysql.tests
 
+import org.jetbrains.squash.dialects.mysql.month
+import org.jetbrains.squash.expressions.literal
+import org.jetbrains.squash.query.select
+import org.jetbrains.squash.results.get
 import org.jetbrains.squash.tests.*
+import org.junit.Test
+import java.time.LocalDate
+import kotlin.test.assertEquals
 
 class MySqlQueryTests : QueryTests(), DatabaseTests by MySqlDatabaseTests() {
     override fun nullsLast(sql: String): String {
         return "ISNULL(${sql.removeSuffix(" DESC")}), $sql"
     }
 
+	@Test
+	fun mysqlDatePartFunctions() {
+		withTables {
+			
+			val query = select {
+				literal(LocalDate.of(2001, 12, 1)).month()
+			}
+			
+			connection.dialect.statementSQL(query).assertSQL {
+				"SELECT MONTH(?)"
+			}
+
+			val month = query.execute().single().get<Int>(0)
+			assertEquals(12, month)
+		}
+	}
 }
